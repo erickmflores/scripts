4df1319ef20ce3b917791cc1af65350b60c8ad26
Romero <alan.romero@leschaco.com>
2020-10-19 21:09:39 +0000

    New script for test

---
+++ Script1.ps1
@@ -0,0 +1,9 @@
+import-module ActiveDirectory 
+$DaysInactive = 183
+$time = (Get-Date).Adddays( - ($DaysInactive))
+$location = "OU=Mexiko,OU=Others,OU=Users,OU=Leschaco,DC=hq,DC=LESCHACO,DC=org" 
+$users = Get-ADUser -SearchBase $location -Filter { LastLogonTimeStamp -lt $time } -Properties LastLogonTimeStamp 
+$users |
+select-object Name, @{Name = "Stamp"; Expression = { [DateTime]::FromFileTime($_.lastLogonTimestamp).ToString('yyyy-MM-dd') } }, UserPrincipalName, Enabled, DistinguishedName, Licenses | sort Name 
+Connect-MsolService
+ForEach ($user in  $users) { Get-MsolUser -UserPrincipalName $user.UserPrincipalName | DisplayName, Licenses }
