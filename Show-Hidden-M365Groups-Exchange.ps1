8623f1059428d4074a8f9bf5b0b0814d076daadc
erickmflores <erickmflores@hotmail.com>
2020-10-19 15:56:04 -0500

    Modified hidden groups script

--- Show-Hidden-M365Groups-Exchange.ps1
+++ Show-Hidden-M365Groups-Exchange.ps1
@@ -1,7 +1,7 @@
 # Show hidden Office 365 groups in Exchange clients (like Outlook)
 Install-Module ExchangeOnlineManagement -Scope CurrentUser
-Connect-ExchangeOnline -UserPrincipalName erick.flores@leschaco.com -ShowProgress $true
-Get-UnifiedGroup -Identity ""
-Set-UnifiedGroup -Identity "" -HiddenFromExchangeClientsEnabled:$false
-$group = Get-UnifiedGroup -Identity ""
+Connect-ExchangeOnline -UserPrincipalName 'INSERT EMAIL HERE' -ShowProgress $true
+Get-UnifiedGroup -Identity "INSERT MICROSOFT 365 GROUP NAME HERE"
+Set-UnifiedGroup -Identity "INSERT MICROSOFT 365 GROUP NAME HERE" -HiddenFromExchangeClientsEnabled:$false
+$group = Get-UnifiedGroup -Identity "INSERT MICROSOFT 365 GROUP NAME HERE"
 $group.HiddenFromExchangeClientsEnabled
